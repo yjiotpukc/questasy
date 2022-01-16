3e4199622523bbbca57a358e9696f07a2db4d5ac
yjiotpukc <yjiotpukc@gmail.com>
2022-01-16 21:53:35 +0300

    Added main game route

--- config/routes/routes.php
+++ config/routes/routes.php
@@ -2,9 +2,11 @@
 
 declare(strict_types=1);
 
+use Game\Presentation\Controller\GameController;
 use Game\Presentation\Controller\HomeController;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return static function (RoutingConfigurator $routing) {
-    $routing->add('home', '/')->controller([HomeController::class, 'home']);
+    $routing->add('home', '/')->methods(['GET'])->controller([HomeController::class, 'home']);
+    $routing->add('game', '/game')->methods(['GET'])->controller([GameController::class, 'questProgress']);
 };
---
+++ src/Game/Presentation/Controller/GameController.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Game\Presentation\Controller;
+
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Game\Domain\Entity\Player;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class GameController extends AbstractController
+{
+    public function questProgress(DocumentManager $dm): Response
+    {
+        $player = $dm->find(Player::class, '61db07efdf36e1609145afd4');
+        $walkthrough = $player->currentWalkthrough;
+
+        if ($walkthrough->currentQuest === null) {
+            $view = 'game/hub.html.twig';
+        } else {
+            $view = 'game/quest.html.twig';
+        }
+
+        return $this->render($view, compact($walkthrough));
+    }
+}
